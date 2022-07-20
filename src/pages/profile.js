d97ebedaeb2305ae6cfe14e3abc7f61086955648
Jacob Allen Valderama <Jacob Allen>
2022-07-21 00:58:09 +0800

    Add popper for changing password

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,12 +1,27 @@
 import {
   Avatar,
   Button,
-  FormControl,
   Grid,
   Stack,
   TextField,
   Typography,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Paper,
+  FormControl,
+  Input,
+  InputLabel,
+  FormHelperText,
+  OutlinedInput,
+  InputAdornment,
 } from '@mui/material';
+import IconButton from '@mui/material/IconButton';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
 import React from 'react';
 import global from '../styles/global';
 import ShelterAdminLayout from '../components/shelterAdminLayout';
@@ -14,6 +29,42 @@ import { deepOrange } from '@mui/material/colors';
 import { Box } from '@mui/system';
 
 export default function Profile() {
+  const [open, setOpen] = React.useState(false);
+
+  // Passwords
+
+  const [values, setValues] = React.useState({
+    amount: '',
+    password: '',
+    weight: '',
+    weightRange: '',
+    showPassword: false,
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // style
   const style = {
     textfield: {
       width: '348px',
@@ -94,9 +145,135 @@ export default function Profile() {
             </Grid>
             <Grid item container xs={4}>
               <Grid item>
-                <Button sx={{ ...global.button1, fontWeight: 'bold' }}>
-                  Change Password
-                </Button>
+                <Box>
+                  <Button
+                    sx={{ ...global.button1, fontWeight: 'bold' }}
+                    onClick={handleClickOpen}
+                  >
+                    ChangePassword
+                  </Button>
+                  <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    PaperProps={{
+                      style: {
+                        borderRadius: 30,
+                        width: '500px',
+                        padding: '20px',
+                      },
+                    }}
+                  >
+                    <DialogTitle sx={{ fontWeight: 'bold', color: '#E94057' }}>
+                      {'CHANGE PASSWORD'}
+                    </DialogTitle>
+                    <DialogContent>
+                      {/* password Old Password */}
+                      <Typography sx={{ fontWeight: 'bold' }}>
+                        Old Password
+                      </Typography>
+                      <FormControl sx={{ m: 1 }} variant="outlined" fullWidth>
+                        <OutlinedInput
+                          type={values.showPassword ? 'text' : 'password'}
+                          value={values.password}
+                          onChange={handleChange('password')}
+                          endAdornment={
+                            <InputAdornment position="end">
+                              <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                              >
+                                {values.showPassword ? (
+                                  <VisibilityOff />
+                                ) : (
+                                  <Visibility />
+                                )}
+                              </IconButton>
+                            </InputAdornment>
+                          }
+                        />
+                      </FormControl>
+                      {/* password Old Password */}
+
+                      {/* password Old Password */}
+                      <Typography sx={{ fontWeight: 'bold' }}>
+                        New Password
+                      </Typography>
+                      <FormControl sx={{ m: 1 }} variant="outlined" fullWidth>
+                        <OutlinedInput
+                          type={values.showPassword ? 'text' : 'password'}
+                          value={values.password}
+                          onChange={handleChange('password')}
+                          endAdornment={
+                            <InputAdornment position="end">
+                              <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                              >
+                                {values.showPassword ? (
+                                  <VisibilityOff />
+                                ) : (
+                                  <Visibility />
+                                )}
+                              </IconButton>
+                            </InputAdornment>
+                          }
+                        />
+                      </FormControl>
+                      {/* password Old Password */}
+                      {/* password Old Password */}
+                      <Typography sx={{ fontWeight: 'bold' }}>
+                        Confirm Password
+                      </Typography>
+                      <FormControl sx={{ m: 1 }} fullWidth variant="outlined">
+                        <OutlinedInput
+                          type={values.showPassword ? 'text' : 'password'}
+                          value={values.password}
+                          onChange={handleChange('password')}
+                          endAdornment={
+                            <InputAdornment position="end">
+                              <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={handleClickShowPassword}
+                                onMouseDown={handleMouseDownPassword}
+                                edge="end"
+                              >
+                                {values.showPassword ? (
+                                  <VisibilityOff />
+                                ) : (
+                                  <Visibility />
+                                )}
+                              </IconButton>
+                            </InputAdornment>
+                          }
+                        />
+                      </FormControl>
+                      {/* password Old Password */}
+                    </DialogContent>
+
+                    <DialogActions
+                      sx={{
+                        display: 'flex',
+                        justifyContent: 'space-evenly',
+                        marginBottom: '12px',
+                      }}
+                    >
+                      <Button onClick={handleClose} sx={{ ...global.button3 }}>
+                        Cancel
+                      </Button>
+                      <Button
+                        onClick={handleClose}
+                        autoFocus
+                        sx={{ ...global.button2Small }}
+                      >
+                        Save
+                      </Button>
+                    </DialogActions>
+                  </Dialog>
+                </Box>
               </Grid>
               <Grid item>
                 <Button sx={{ ...global.button2, fontWeight: 'bold' }}>
--- src/styles/global.js
+++ src/styles/global.js
@@ -5,6 +5,7 @@ const global = {
     width: '348px',
     background: '#ADFF00',
     borderRadius: '20px',
+    fontWeight: 'bold',
   },
   button2: {
     background: '#E94057',
@@ -12,6 +13,25 @@ const global = {
     padding: '20px',
     width: '348px',
     borderRadius: '20px',
+    fontWeight: 'bold',
+  },
+  button2Small: {
+    background: '#E94057',
+    color: '#333',
+    padding: '20px',
+    borderRadius: '20px',
+    height: ' 51px',
+    width: ' 173px',
+    fontWeight: 'bold',
+  },
+  button3: {
+    color: '#333',
+    padding: '20px',
+    background: '#D4D4D4',
+    borderRadius: '20px',
+    fontWeight: 'bold',
+    height: ' 51px',
+    width: ' 173px',
   },
 };
 export default global;
